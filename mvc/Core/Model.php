b223e2692aa4e1834cdc56c81d2b9e9a8685f115
LinhTVK <88108412+LinhTVK@users.noreply.github.com>
2021-11-05 10:22:26 +0700

    Update Model.php

--- mvc/Core/Model.php
+++ mvc/Core/Model.php
@@ -1,7 +1,16 @@
 <?php
 namespace MVC\Core;
+	/**
+    * Class Model
+    */
     class Model
     {
+    	/**
+	    * Get Properties
+	    *
+	    * @param \Core\ResourcekModel
+	    * @return Boolean
+	    */
         public function getProperties() {
             return get_object_vars($this);
         }
