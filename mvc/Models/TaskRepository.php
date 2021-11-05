faed7175d139644f1660d0eb26893b413ce325eb
LinhTVK <88108412+LinhTVK@users.noreply.github.com>
2021-11-05 10:14:22 +0700

    Update TaskRepository.php

--- mvc/Models/TaskRepository.php
+++ mvc/Models/TaskRepository.php
@@ -4,6 +4,9 @@ use MVC\Core\Model;
 use MVC\Config\Database;
 use MVC\Models\TaskResourceModel;
 use MVC\Models\TaskModel;
+	/**
+	* Class TaskRepository
+	*/
     class TaskRepository {
     	private $taskresource;
     	public function __construct()
@@ -11,23 +14,47 @@ use MVC\Models\TaskModel;
 	        $this->taskresource = new TaskResourceModel();
 	    }
 
+	    /**
+	    * Save Block data
+	    *
+	    * @param \MVC\Models\TaskModel $model
+	    * @return Boolean
+	    */
 	    public function add(TaskModel $model)
 	    {
 	    	// die("anf");
 	        return $this->taskresource->save($model);
 	    }
 
-	    public function get(int $id)
+	    /**
+	    * Save Block data
+	    *
+	    * @param \MVC\Models\TaskModel $id
+	    * @return Boolean
+	    */
+	    public function get(int $id): TaskModel
 	    {
 	        return $this->taskresource->get($id);
 	    }
 
-	    public function getAll()
+	    /**
+	    * Save Block data
+	    *
+	    * @param \MVC\Models\TaskModel[]
+	    * @return Boolean
+	    */
+	    public function getAll(): array
 	    {
 	        return $this->taskresource->getAll();
 	    }
 
-	    public function delete($id)
+	    /**
+	    * Save Block data
+	    *
+	    * @param \MVC\Models\TaskModel $id
+	    * @return Boolean
+	    */
+	    public function delete($id): TaskModel
 	    {
 	        return $this->taskresource->delete($id);
 	    }
