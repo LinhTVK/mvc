a4af48a3ef346fb0f56c6f157be0ace11ee3ad83
LinhTVK <88108412+LinhTVK@users.noreply.github.com>
2021-11-05 10:23:11 +0700

    Update ResourceModelInterface.php

--- mvc/Core/ResourceModelInterface.php
+++ mvc/Core/ResourceModelInterface.php
@@ -1,9 +1,34 @@
 <?php
 namespace MVC\Core;
+	/**
+	* Task CRUD Interface
+	*/
     interface ResourceModelInterface
     {
+    	/**
+        * Standard resource model initialization
+        *
+        * @param string $table
+        * @param string $id
+        * @param object $model
+        * @return void
+        */
         public function _init($table,$id,$model);
+
+        /**
+        * Standard resource model save
+        *
+        * @param object $model
+        * @return void
+        */
         public function save($model);
+
+        /**
+        * Standard resource model delete
+        *
+        * @param object $model
+        * @return void
+        */
         public function delete($model);
     }
 ?>
